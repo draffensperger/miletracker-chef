3f4b1777cee3755adae977da4f38b93ef1835181
David Raffensperger <contact@davidraff.com>
2014-05-26 21:38:52 -0400

    Initial server setup scripts

---
+++ .chef/knife.rb
@@ -0,0 +1,4 @@
+knife[:solo] = true
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
---
+++ Gemfile
@@ -0,0 +1,5 @@
+gem 'chef'
+gem 'knife-solo',
+    :github => 'matschaffer/knife-solo',
+    :branch => 'master',
+    :submodules => true
