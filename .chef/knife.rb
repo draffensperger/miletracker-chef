55aae0b28c2e5d44d89abed1e8b8d9e0dc33e062
David Raffensperger <contact@davidraff.com>
2014-05-30 22:07:12 -0400

    Moved Berkshelf path out of repo

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -8,9 +8,6 @@ node_path        "nodes"
 role_path        "roles"
 environment_path "environments"
 data_bag_path    "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
-
-knife[:berkshelf_path] = "cookbooks"
 
 knife[:secret_file] = ".chef/encrypted_data_bag_secret"
 encrypted_data_bag_secret ".chef/encrypted_data_bag_secret"
--- cookbooks/dokku/recipes/ssh_keys.rb
+++
@@ -1,9 +0,0 @@
-node['dokku']['ssh_keys'].each do |user, key|
-  # TODO make this into an LWRP
-  bash "sshcommand_acl-add_key" do
-    cwd node['dokku']['root']
-    code <<-EOT
-      echo '#{key}' | sshcommand acl-add dokku #{user}
-    EOT
-  end
-end
